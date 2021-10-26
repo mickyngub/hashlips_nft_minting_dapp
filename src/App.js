5379271f957647f8c97437797fab54c32c3be3f9
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-10-26 18:42:43 +0700

    Moved WrapperHomePage to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import { Switch, Route, Redirect } from "react-router-dom";
 import Mint from "./Mint";
-import WrapperHomePage from "./Pages/HomePage/WrapperHomePage";
+import HomePage from "./Pages/HomePage/HomePage";
+
+import Nav from "./components/layout/Nav";
+import Footer from "./components/layout/Footer";
+import style from "./assets/styles/main.module.css";
+
+import discord from "./assets/images/discord.png";
+import twitter from "./assets/images/twitter.png";
 const App = () => {
+  const onProps = {
+    socials: [
+      //   {
+      //     id: 1,
+      //     image: useImage('/medium.png'),
+      //     url: 'https://discord.gg/gEnvhUGh58',
+      //     text: 'meow to the moon medium',
+      //   },
+      {
+        id: 2,
+        image: discord,
+        url: "https://discord.gg/gEnvhUGh58",
+        text: "meow to the moon discord",
+      },
+      {
+        id: 3,
+        image: twitter,
+        url: "https://twitter.com/Meow2themoonNFT",
+        text: "meow to the moon twitter",
+      },
+    ],
+  };
   return (
-    <Switch>
-      <Route path="/" exact component={WrapperHomePage} />
-      <Route path="/mint" exact component={Mint} />
-      <Redirect to="/" />
-    </Switch>
+    <div className={style[`main-layout`]}>
+      <Nav onProps={onProps} />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/mint" exact component={Mint} />
+        <Redirect to="/" />
+      </Switch>
+      <Footer onProps={onProps} />
+    </div>
   );
 };
 
