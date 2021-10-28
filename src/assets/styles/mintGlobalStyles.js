2e2a52373e784ac50f840f06ceb7180f03dd4127
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-10-28 16:07:56 +0700

    Update mintGlobalStyles.js

--- src/assets/styles/mintGlobalStyles.js
+++ src/assets/styles/mintGlobalStyles.js
@@ -2,10 +2,9 @@ import styled from "styled-components";
 
 // Used for wrapping a page component
 export const Screen = styled.div`
-  background-color: var(--primary);
   background-image: ${({ image }) => (image ? `url(${image})` : "none")};
   background-size: cover;
-  background-position: center;
+  background-repeat: no-repeat;
   width: 100%;
   min-height: 100vh;
   display: flex;
@@ -43,7 +42,6 @@ export const Container = styled.div`
   flex-direction: ${({ fd }) => (fd ? fd : "column")};
   justify-content: ${({ jc }) => (jc ? jc : "flex-start")};
   align-items: ${({ ai }) => (ai ? ai : "flex-start")};
-  background-color: ${({ test }) => (test ? "pink" : "none")};
   width: 100%;
   background-image: ${({ image }) => (image ? `url(${image})` : "none")};
   background-size: cover;
