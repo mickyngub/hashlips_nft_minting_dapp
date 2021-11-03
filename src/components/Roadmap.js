31ca54e92d7380fb9e90951e4e65e326e8a155f4
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-11-03 18:35:40 +0700

    Change roadmap / add react-html-parser / change roadmap color

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -1,4 +1,9 @@
 import React from "react";
+import ReactHtmlParser, {
+  processNodes,
+  convertNodeToElement,
+  htmlparser2,
+} from "react-html-parser";
 
 export default function RoadMap() {
   function classList(classes) {
@@ -18,8 +23,12 @@ export default function RoadMap() {
               active: item.is_active,
             })}
           >
-            <h3 className="text--xl text--700">{item.title}</h3>
-            <p className="text--xl desc pre-white">{item.text}</p>
+            <h3 style={{ color: "white" }} className="text--xl text--700">
+              {item.title}
+            </h3>
+            <p style={{ color: "#AEB6C5" }} className="text--xl desc pre-white">
+              {ReactHtmlParser(item.text)}
+            </p>
 
             <div className="timeline-item-number">
               <p className="text--sm text--700">Episode</p>
@@ -56,16 +65,22 @@ export default function RoadMap() {
             <p className="text--sm text--700">Episode</p>
             <p className="text--body text--700">{item.position}</p>
           </div>
-          <p className="roadMap-sm-title-text">{item.title}</p>
+          <p style={{ color: "white" }} className="roadMap-sm-title-text">
+            {item.title}
+          </p>
         </div>
-        <p className="text--xl desc text--left ">{item.text}</p>
+        <p style={{ color: "#AEB6C5" }} className="text--xl desc text--left ">
+          {item.text}
+        </p>
       </div>
     ));
   }
   return (
     <section id="roadMap" className="roadMap-overview">
       <div className="roadMap-wrapper">
-        <h2 className="text--5xl">Roadmap</h2>
+        <h2 style={{ color: "white" }} className="text--5xl">
+          Roadmap
+        </h2>
         <div className="timeline-wrapper">
           {RoadMapDesktop()}
           {RoadMapMobile()}
@@ -79,24 +94,24 @@ const ITEMS = [
   {
     id: 1,
     header: {
-      title: "Season 1",
-      text: "The beginning of the Moon Journey",
+      title: "Season 0",
+      text: "The fall and emergence of The Meow Genesis",
     },
-    title: "The Crew Recruitment",
-    text: "The meow army wants to onboard the marvelous soldier to join the spaceship. There will be 2 types of on-board: Whitelist and Public Stealth Launch",
-    position: "1",
+    title: "Prologue",
+    text: "The gas blackhole has damaged the spaceship; only 187 meows survived from the incident. These survivors are regarded as <b class='redText'> “The Meow Genesis”. </b>",
+    position: "0",
     is_active: true,
   },
   {
     id: 2,
     header: {
-      title: "",
-      text: "",
+      title: "Season 1",
+      text: "The New Beginning",
     },
-    title: "The Visit of Land (777,777)",
-    text: "The onboarding is done; now, the first journey of the meows is the land of trade, Openseas.  After two weeks of listing, the holders of Meow will receive a raffle every two weeks to participate in the 777 Meow machine. The machine is the slot machine where the player will be guaranteed a reward; however, it’s all up to your luck how much you would receive. The fund of the slot machine will be 10% of our minting margin, which the game will continue until it runs out.",
-    position: "2",
-    is_active: false,
+    title: "The Awakening of Genesis",
+    text: "Brace for the impact, a huge wave is coming. The 187 uniquely 1/1 created Meow will be revealed; the new era of NFT is here.",
+    position: "1",
+    is_active: true,
   },
   {
     id: 3,
@@ -104,22 +119,9 @@ const ITEMS = [
       title: "",
       text: "",
     },
-    title: "The Treasure Cave",
-    text: `During the expenditure in the land of trade, the Meow’s holder will be able to stake their Meow NFT to receive the $Meow Token which can be used in Meow’s cave, an exclusive reward center where holder can exchange their $Meow Token for: 
-    
-    [Exclusive merchandise]:  Hoodie, Smooshie Meow Pillow, Meow Figure, and etc
-
-    [DAO’s Selection]: Anything purchased by the Dao’s wallet 
-
-    [Fast-pass comic ticket]: One-week fast pass
-    
-    [The Secret Ticket]: The pass to the secret collection
-
-    The royalty fee from the secondary marketplace will be distributed accordingly, 
-    30% for marketing, 30% to the DAO wallet, 20% to sweep the floor, 10% to charity upon community selection, and 10% for hiring new members.
-    
-    In addition, the start of Meow to the Moon The Serie comic will be available for holders to read every week.`,
-    position: "3",
+    title: "The Attention",
+    text: "The world is panicking from the “Gas blackhole Incident”, living in fear and anxiety. The team will focus hugely on marketing through influencers, social media, and bring the mass attention to ramp up Meow to the Moon into space.",
+    position: "2",
     is_active: false,
   },
   {
@@ -128,33 +130,94 @@ const ITEMS = [
       title: "",
       text: "",
     },
-    title: "The Doggy Attack",
-    text: `The spaceship has been attacked by the doggy army.What should we do? The MVP of the war will receive a special airdrop from the Meow’s Team. The airdrop will be the animated version of Meow to the Moon NFT. 
-
-    The holders will have the opportunity to participate in a mini-game, in which your meow will be used as the persona to participate in the game. Will you be the winner?
-    `,
-    position: "4",
+    title: "The Genesis’s Right",
+    text: "Two days Before the pre-sale of Meow V2, <b class='redText'>The Meow Genesis holder will be able to free mint 2 Meows V2 for every Meow Genesis they hold.</b>",
+    position: "3",
     is_active: false,
   },
   {
     id: 5,
     header: {
       title: "Season 2",
-      text: "The Warp Port",
+      text: "The V2 Era",
     },
-    title: "Episode X",
-    text: `In the last chapter of Meow to the Moon Season 1, there will be a cliffhanger. The cliffhanger will revolve around the Meow Secret Pass. For the secret pass holder, you will have the access for a spot in this Season 2 journey. Are you ready for the next battle? 
+    title: "The Meow V2 Recruitment (Date: TBA)",
+    text: `The meow army wants to recruit marvelous soldiers to join the spaceship. The new Meow V2 collection will be launched.
     
-    [Sandbox Land] - We will purchase land, which will serve as the place where Meow’s holders can engage with each other, build connections, and promote their works. 
-
-    [$Meow Token V2] - We will expand the usage of $Meow token beyond the reward center in season 1. It may be in the format of NFT-game, Utility Usage, and etc, depending on the direction that the community wants us to go.
+    <b class='redText'>- The Meow Genesis Holder will automatically receive the Whitelist in the Meow V2 Collection</b>
+    `,
+    position: "1",
+    is_active: true,
+  },
+  {
+    id: 6,
+    header: {
+      title: "",
+      text: "",
+    },
+    title: "The 777 Meow Machine (At Meow V2 25% Sold)",
+    text: `Meow V2 holders will receive a raffle for every meow V2 they own <b class='redText'> + Meow Genesis holders will receive an addition of 1 raffle </b> for every two weeks to participate in the 777 Meow machine.
     
-    Season 2 Collection: Be sure to follow Meow To the Moon The Serie 
-
-    Meow Note: We are only as big as our community, the community will be the one who decides the direction of Season 2.
+    The 777 Meow machine is the slot machine where the player will be <b>guaranteed a reward</b>, with the fund of the slot machine will be <b>10% of our Meow V2 minting revenue</b>
 
     `,
-    position: "x",
+    position: "2",
+    is_active: false,
+  },
+  {
+    id: 7,
+    header: {
+      title: "",
+      text: "",
+    },
+    title: "The Treasure Cavemoon (At Meow V2 50% Sold)",
+    text: `The Meow Genesis and Meow V2 holders will be able to <b>stake Meow NFT</b> to receive the <b>$Meow Token</b> which can be used in Meow’s cave, an exclusive reward center.
+
+    <b class='redText'>The Genesis Meow Collection will have x1.5 multiplier to the earning of $Meow Token</b>.
+
+    <b>Reward</b> -
+    <b>[DAO’s Selection]</b>: Anything purchased by the Dao’s wallet
+    <b>[Fast-pass comic ticket]</b>: One-week fast pass 
+    <b>[The Secret Ticket]</b>: The pass to the <b>Secret Collection</b>...
+    <b class='redText'>[Genesis’s exclusive merchandise]: Only for Meow Genesis holder for free</b>
+
+    In addition, the start of <b>Meow to the Moon The Series</b> comic will be available for holders to read every week.
+
+    `,
+    position: "3",
+    is_active: false,
+  },
+  {
+    id: 8,
+    header: {
+      title: "",
+      text: "",
+    },
+    title: "The Doggy Attack (At Meow V2 75% Sold)",
+    text: `The spaceship has been attacked by the doggy army. What should we do? The MVP of the war will receive a special airdrop from the Meow’s Team, <b>a playable Meow NFT in Sandbox Game</b>
+
+    The holders will have the opportunity to participate in a mini-game, which the trait of your Meow will be contributed to the performance in the game
+    
+    `,
+    position: "3.5",
+    is_active: false,
+  },
+  {
+    id: 9,
+    header: {
+      title: "Season 3",
+      text: "The Warp Port (At Meow V2 100% sold)",
+    },
+    title: "Epilogue",
+    text: `<b>[Sandbox Land]</b> - We will purchase land, which will serve as the place where Meow’s holders can engage with each other, build connections, and promote their works.
+    <b class="redText">Genesis Holder will be given 1x Genesis House in the Meow’s Land we are going to build in the Sandbox Metaverse. </b>
+  .
+  <b>[Meow Token V2]</b> - We will expand the usage of Meow tokens beyond the reward center in season 1. It may be in the format of NFT-game, Utility Usage, and etc, depending on the direction that the community wants us to go.
+  .
+  <b>[Meow Note]</b> - We are only as big as our community, the community will be the one who decides the direction of Season 3.
+  
+    `,
+    position: "X",
     is_active: true,
   },
 ];
